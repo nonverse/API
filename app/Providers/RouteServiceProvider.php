a453b36d76b4b4db35dd09a4723a8e754e46e511
Isuru Abhayaratne <isuru2003a@gmail.com>
2021-10-31 01:37:47 +1100

    Register auth route class

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -38,10 +38,10 @@ class RouteServiceProvider extends ServiceProvider
         $this->configureRateLimiting();
 
         $this->routes(function () {
-            Route::prefix('api')
+            Route::prefix('auth')
                 ->middleware('api')
                 ->namespace($this->namespace)
-                ->group(base_path('routes/api.php'));
+                ->group(base_path('routes/auth.php'));
 
             Route::middleware('web')
                 ->namespace($this->namespace)
