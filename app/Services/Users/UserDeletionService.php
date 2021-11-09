969c29024f571da47f22172984035280018114dc
Isuru Abhayaratne <isuru2003a@gmail.com>
2021-11-09 17:51:02 +1100

    Create base user deletion service

---
+++ app/Services/Users/UserDeletionService.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Services\Users;
+
+use App\Contracts\Repository\UserRepositoryInterface;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Contracts\Hashing\Hasher;
+use PragmaRX\Google2FA\Google2FA;
+
+class UserDeletionService
+{
+    /**
+     * @var UserRepositoryInterface
+     */
+    private $repository;
+
+    public function __construct(
+        UserRepositoryInterface $repository
+    )
+    {
+        $this->repository = $repository;
+    }
+
+    public function handle($uuid): bool
+    {
+        return $this->repository->delete($uuid);
+    }
+}
