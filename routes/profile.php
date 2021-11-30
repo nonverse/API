a06658c4f513e4f02c487b3a8588cce714b14f10
Isuru Abhayaratne <isuru2003a@gmail.com>
2021-12-01 00:39:30 +1100

    Create controller to verify a user's profile creation data and if valid, send a one time password

---
+++ routes/profile.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| User profile API routes that can only be accessed by a session authenticated
+| user via an app on the .nonverse.net domain
+|--------------------------------------------------------------------------
+| Endpoint: /user/profile
+|
+*/
+
+// Auth required
+Route::group(['middleware' => 'auth'], function () {
+    Route::post('/verify/send', [\App\Http\Controllers\Profile\ProfileVerificationController::class, 'sendVerification']);
+});
+
+// Auth or API token required
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    //
+});
