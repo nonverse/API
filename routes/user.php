24dafa648a719cef85c029a127487c1335019c71
Isuru Abhayaratne <isuru2003a@gmail.com>
2021-12-05 01:31:45 +1100

    Create post route to update user password

--- routes/user.php
+++ routes/user.php
@@ -20,8 +20,8 @@ Route::get('/', function() {
 
 // Auth required
 Route::group(['middleware' => 'auth'], function () {
-    // Update a user
-    Route::post('store', [\App\Http\Controllers\UserController::class, 'update']);
+    // Update a user's password
+    Route::post('store/password', [\App\Http\Controllers\UserController::class, 'updatePassword']);
     // Delete a user's store
     Route::delete('store', [\App\Http\Controllers\UserController::class, 'delete']);
 
