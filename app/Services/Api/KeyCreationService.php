a02b70f41d639c3d98870a59063109ca4747780b
Isuru Abhayaratne <isuru2003a@gmail.com>
2021-12-12 18:51:18 +1100

    Create service to create a new API Key for a user

---
+++ app/Services/Api/KeyCreationService.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Services\Api;
+
+use App\Contracts\Repository\UserRepositoryInterface;
+
+class KeyCreationService
+{
+
+    /**
+     * @var UserRepositoryInterface
+     */
+    private $repository;
+
+    public function __construct(
+        UserRepositoryInterface $repository
+    )
+    {
+        $this->repository = $repository;
+    }
+
+    /**
+     * @param $uuid
+     * @param array $data
+     * @return array
+     */
+    public function handle($uuid, array $data): array
+    {
+        $user = $this->repository->get($uuid);
+        $token = $user->createToken($data['key_name'], $data['permissions']);
+
+        return array(
+            'token_id' => explode('|', $token->plainTextToken)[0],
+            'token_value' => explode('|', $token->plainTextToken)[1]
+        );
+    }
+}
