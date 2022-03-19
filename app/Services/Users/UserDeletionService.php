92b82adea8f11d30d34999191372364697b2e008
isuru <isuru2003a@gmail.com>
2022-03-20 03:42:43 +1100

    Delete any invited used by a user before deleting their account

--- app/Services/Users/UserDeletionService.php
+++ app/Services/Users/UserDeletionService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services\Users;
 
+use App\Contracts\Repository\InviteRepositoryInterface;
 use App\Contracts\Repository\UserProfileRepositoryInterface;
 use App\Contracts\Repository\UserRepositoryInterface;
 
@@ -17,21 +18,35 @@ class UserDeletionService
      */
     private $profileRepository;
 
+    /**
+     * @var InviteRepositoryInterface
+     */
+    private $inviteRepository;
+
     public function __construct(
-        UserRepositoryInterface $repository,
-        UserProfileRepositoryInterface $profileRepository
+        UserRepositoryInterface        $repository,
+        UserProfileRepositoryInterface $profileRepository,
+        InviteRepositoryInterface      $inviteRepository
     )
     {
         $this->repository = $repository;
         $this->profileRepository = $profileRepository;
+        $this->inviteRepository = $inviteRepository;
     }
 
     //TODO Include logic to check for profile and delete other user data
     public function handle($uuid): bool
     {
+        $user = $this->repository->get($uuid);
+
         // Delete a user's profile if one exists
         if ($this->profileRepository->get($uuid)) {
             $this->profileRepository->delete($uuid);
+        }
+
+        // Delete any invites used by the user
+        if ($this->inviteRepository->get($user->email)) {
+            $this->inviteRepository->delete($user->email);
         }
 
         // Delete a user's account store
