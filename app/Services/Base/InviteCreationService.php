a84ce638150a374621a318826041d7ae9961e494
isuru <isuru2003a@gmail.com>
2022-03-19 23:53:55 +1100

    Create service to generate an account activation key and send it to a user when they are invited

---
+++ app/Services/Base/InviteCreationService.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Services\Base;
+
+use App\Contracts\Repository\InviteRepositoryInterface;
+use App\Notifications\Invited;
+use Carbon\CarbonImmutable;
+use Illuminate\Http\Request;
+use Illuminate\Contracts\Hashing\Hasher;
+use Illuminate\Support\Facades\Notification;
+use Illuminate\Support\Str;
+use Exception;
+
+class InviteCreationService
+{
+    /**
+     * @var InviteRepositoryInterface
+     */
+    private $repository;
+
+    /**
+     * @var Hasher
+     */
+    private $hasher;
+
+    public function construct(
+        InviteRepositoryInterface $repository,
+        Hasher                    $hasher
+    )
+    {
+        $this->repository = $repository;
+        $this->hasher = $hasher;
+    }
+
+    public function handle(Request $request, $email)
+    {
+        $key = implode('-', str_split(strtoupper(Str::random(16)), 4));
+
+        $this->repository->create([
+            'email' => $email,
+            'invite_key' => $this->hasher->make($key),
+            'invited_by' => $request->user()->uuid,
+            'key_expiry' => CarbonImmutable::now()->addDays(30)
+        ]);
+
+        try {
+            Notification::route('mail', $email)
+                ->notify(new Invited([
+                    'key' => $key,
+                    'email' => $email
+                ]));
+
+            return true;
+        } catch (Exception $e) {
+            $this->repository->delete($email);
+            return false;
+        }
+    }
+}
