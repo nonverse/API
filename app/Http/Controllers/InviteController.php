31a6d190a992ea4dbca6d725e7829ddb0235cb72
isuru <isuru2003a@gmail.com>
2022-03-23 17:00:53 +1100

    Create controller function to delete invites

--- app/Http/Controllers/InviteController.php
+++ app/Http/Controllers/InviteController.php
@@ -97,4 +97,41 @@ class InviteController extends Controller
     {
         return $this->repository->index();
     }
+
+    /**
+     * Delete an invite
+     *
+     * @param Request $request
+     * @param $email
+     * @return JsonResponse|void
+     */
+    public function delete(Request $request,$email)
+    {
+        $invite = $this->repository->get($email);
+        // Ensure that the invite has not been claimed
+        if ($invite->claimed_by) {
+            return new JsonResponse([
+                'errors' => [
+                    'invite' => 'Cannot withdraw claimed invites'
+                ],
+            ], 400);
+        }
+
+        // Ensure that the currently logged in user created the invite
+        if ($invite->invited_by !== $request->user()->uuid) {
+            return new JsonResponse([
+                'errors' => [
+                    'invite' => 'Cannot withdraw invites created by other admins'
+                ],
+            ], 403);
+        }
+
+        if ($this->repository->delete($email)) {
+            return new JsonResponse([
+                'data' => [
+                    'success' => true
+                ]
+            ]);
+        }
+    }
 }
