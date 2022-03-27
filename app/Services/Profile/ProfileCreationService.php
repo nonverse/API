a09ef6162579d2c8223988b93f6b7430b8a6ca8d
isuru <isuru2003a@gmail.com>
2022-03-27 21:59:54 +1100

    Create user's AuthMe upon profile link

--- app/Services/Profile/ProfileCreationService.php
+++ app/Services/Profile/ProfileCreationService.php
@@ -2,9 +2,11 @@
 
 namespace App\Services\Profile;
 
+use App\Contracts\Repository\AuthMeRepositoryInterface;
 use App\Contracts\Repository\UserProfileRepositoryInterface;
 use App\Contracts\Repository\UserRepositoryInterface;
 use App\Models\Profile;
+use App\Models\User;
 use Carbon\CarbonImmutable;
 use Illuminate\Support\Facades\Http;
 
@@ -21,14 +23,21 @@ class ProfileCreationService
      */
     private $userRepository;
 
+    /**
+     * @var AuthMeRepositoryInterface
+     */
+    private $authMeRepository;
+
     public function __construct
     (
         UserProfileRepositoryInterface $profileRepository,
-        UserRepositoryInterface $userRepository
+        UserRepositoryInterface        $userRepository,
+        AuthMeRepositoryInterface      $authMeRepository
     )
     {
         $this->profileRepository = $profileRepository;
         $this->userRepository = $userRepository;
+        $this->authMeRepository = $authMeRepository;
     }
 
     /**
@@ -42,14 +51,18 @@ class ProfileCreationService
     {
         $response = Http::get('https://api.mojang.com/users/profiles/minecraft/' . $mc_username);
         $user = $this->userRepository->get($uuid);
-        $profile =  $this->profileRepository->create($uuid, array(
+        $profile = $this->profileRepository->create($uuid, array(
             'uuid' => $uuid,
             'mc_uuid' => $response['id'],
             'mc_username' => $response['name'],
             'profile_verified_at' => CarbonImmutable::now()
         ));
+        $authme = $this->authMeRepository->create($uuid, [
+            'mc_username' => $response['name'],
+            'password_hash' => User::query()->find($uuid)->password
+        ]);
 
-        if ($profile) {
+        if ($profile && $authme) {
             // Send confirmation of linking to user
             Http::withToken(env('PANEL_ADMIN_KEY'))->post('https://' . env('PANEL_APP') . '/api/client/servers/' . env('MINECRAFT_LOBBY_SERVER') . '/command', [
                 'command' => 'tellraw ' . $response['name'] . ' ["",{"text":"Your profile has successfully been linked to a Nonverse account with email ","bold":true,"color":"green"},{"text":"' . $user->email . '","color":"gold"}]'
