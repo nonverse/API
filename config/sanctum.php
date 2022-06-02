39481bccfdd4179d3928f8992cb2b64aff221f27
isuru <isuru2003a@gmail.com>
2022-06-02 22:15:28 +1000

    Update application configuration to allow API to be stateful

--- config/sanctum.php
+++ config/sanctum.php
@@ -15,8 +15,8 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'auth.nonverse.test',
-        env('APP_URL') ? ','.parse_url(env('APP_URL'), PHP_URL_HOST) : ''
+        '',
+        env('APP_URL') ? ',' . parse_url(env('APP_URL'), PHP_URL_HOST) : ''
     ))),
 
     /*
