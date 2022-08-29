ca808bb57bf23d2c7ca37304a5a0c6768b6b6e39
Isuru Abhayaratne <isuru2003a@gmail.com>
2022-08-29 16:03:40 +1000

    Customise passport migrations to user UUIDs for user-id

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'core'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ return [
 
     'connections' => [
 
-        'core' => [
+        'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
