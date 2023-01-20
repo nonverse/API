8d5451c20baf698ce6d28fa2d9310ade6f32478e
Isuru Abhayaratne <isuru2003a@gmail.com>
2023-01-20 14:11:09 +1100

    Create user routes to handle basic user related requests (get, create, update, delete)

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::prefix('user')->group(base_path('routes/user.php'));
---
+++ routes/user.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| User API Routes
+|--------------------------------------------------------------------------
+|
+| Endpoint: /api/user
+|
+*/
+
+// Create new user
+Route::post('/', [\App\Http\Controllers\User\UserController::class, 'store']);
+
+/**
+ * Authentication required
+ */
+Route::group([], function() {
+    // Get user store
+    Route::get('/store', [\App\Http\Controllers\User\UserController::class, 'get']);
+    // Update user store
+    Route::post('/store', [\App\Http\Controllers\User\UserController::class, 'update']);
+    // Delete user store
+    Route::delete('/store',  [\App\Http\Controllers\User\UserController::class, 'delete']);
+});
