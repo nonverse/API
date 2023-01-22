7793b8d07632113217e17ff423d451a6d32ad0f6
Isuru Abhayaratne <isuru2003a@gmail.com>
2023-01-22 12:43:17 +1100

    Passport config

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
-    use HasFactory, Notifiable, SoftDeletes;
+    use HasApiTokens ,HasFactory, Notifiable, SoftDeletes;
 
     /**
      * The primary key associated with the table
