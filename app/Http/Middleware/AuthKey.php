4aab34269f01018cef2a24f76dc11285491d8d39
Isuru <isuru2003a@gmail.com>
2023-05-27 17:56:21 +1000

    Create middleware to authorise auth server

---
+++ app/Http/Middleware/AuthKey.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthKey
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next): \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+    {
+        if (!Hash::check($request->bearerToken(), env('AUTH_KEY_HASH'))) {
+            abort(401);
+        }
+        return $next($request);
+    }
+}
