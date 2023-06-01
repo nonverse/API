bbec93b264f6d3b1033a75d626fdb9b0f82b98f1
Isuru <isuru2003a@gmail.com>
2023-06-01 17:33:09 +1000

    Remove Laravel passport

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,11 +7,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Passport\HasApiTokens;
-
 class User extends Authenticatable implements MustVerifyEmail
 {
-    use HasApiTokens ,HasFactory, Notifiable, SoftDeletes;
+    use HasFactory, Notifiable, SoftDeletes;
 
     /**
      * The primary key associated with the table
