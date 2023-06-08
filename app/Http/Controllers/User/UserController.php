3880bbcf9a5105c960e1b31ad1138bd117d47bfd
Isuru <isuru2003a@gmail.com>
2023-06-08 19:32:40 +1000

    Only allow user's basic details to be updates via this controller. Other meta details should be updated based on their specific requirements

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -8,11 +8,9 @@ use App\Services\User\UserCreationService;
 use App\Services\User\UserDeletionService;
 use App\Services\User\UserUpdateService;
 use Exception;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Js;
 use Illuminate\Validation\ValidationException;
 
 class UserController extends Controller
@@ -114,8 +112,8 @@ class UserController extends Controller
         $user = $request->user();
 
         $validator = Validator::make($request->input(), [
-            'email' => 'email:rfc,dns|unique:users,email,'.$user->uuid.',uuid',
-            'username' => 'unique:users,username,'.$user->uuid.',uuid',
+            'email' => 'email:rfc,dns|unique:users,email,' . $user->uuid . ',uuid',
+            'username' => 'unique:users,username,' . $user->uuid . ',uuid',
             'phone' => 'min:7|max:15|nullable',
             'dob' => 'nullable',
             'password' => 'min:8'
@@ -132,7 +130,9 @@ class UserController extends Controller
             ], 422);
         }
 
-        $user = $this->updateService->handle($request->user()->uuid, $request->input());
+        $user = $this->updateService->handle($request->user()->uuid, $request->only(
+            ['name_first', 'name_last', 'username', 'email', 'phone', 'dob']
+        ));
 
         return new JsonResponse([
             'success' => true,
