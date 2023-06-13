dc20df72917e4a66403ecc38e694fbd092f5ee70
Isuru <isuru2003a@gmail.com>
2023-06-13 23:38:34 +1000

    Bind recovery repository

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use App\Contracts\Repository\RecoveryRepositoryInterface;
 use App\Contracts\Repository\RepositoryInterface;
 use App\Contracts\Repository\UserRepositoryInterface;
+use App\Repositories\RecoveryRepository;
 use App\Repositories\Repository;
 use App\Repositories\UserRepository;
 use Illuminate\Support\ServiceProvider;
@@ -14,5 +16,6 @@ class RepositoryServiceProvider extends ServiceProvider
     {
         $this->app->bind(RepositoryInterface::class, Repository::class);
         $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
+        $this->app->bind(RecoveryRepositoryInterface::class, RecoveryRepository::class);
     }
 }
