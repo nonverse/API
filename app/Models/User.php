41c17cc5340dc3c5b7813d55d1b7f6cc5f4f0fb1
Isuru <isuru2003a@gmail.com>
2023-06-13 23:38:44 +1000

    Make user 'gender' column fillable

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,6 +46,7 @@ class User extends Authenticatable implements MustVerifyEmail
         'email',
         'phone',
         'dob',
+        'gender',
         'password',
         'admin',
         'restrictions',
