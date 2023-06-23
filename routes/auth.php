675afb85c2c36629245b5653decc4b13a54f8dc0
Isuru <isuru2003a@gmail.com>
2023-06-24 00:41:35 +1000

    Create route to send verification codes

--- routes/auth.php
+++ routes/auth.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,5 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::prefix('user')->group(base_path('routes/user.php'));
-Route::prefix('auth')->group(base_path('routes/auth.php'));
+Route::post('send-verification');
