e0ef6b385e7609f44904181943db8badd31a03df
Isuru <isuru2003a@gmail.com>
2023-06-24 00:41:26 +1000

    Create controller to send verification codes

---
+++ app/Http/Controllers/VerificationController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\Phone\SendPhoneVerificationCodeService;
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class VerificationController extends Controller
+{
+    /**
+     * @var SendPhoneVerificationCodeService
+     */
+    private SendPhoneVerificationCodeService $sendPhoneVerificationCodeService;
+
+    public function __construct(
+        SendPhoneVerificationCodeService $sendPhoneVerificationCodeService
+    )
+    {
+        $this->sendPhoneVerificationCodeService = $sendPhoneVerificationCodeService;
+    }
+
+    /**
+     * Handle request to send verification code to phone number
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function send(Request $request): JsonResponse
+    {
+        $validator = Validator::make($request->all(), [
+            'channel' => 'required',
+            'phone' => 'required_without:email|min:7|max:15|prohibits:email',
+            'email' => 'required_without:phone|email:rfc,dns|prohibits:phone'
+        ]);
+
+        if ($validator->fails()) {
+            return new JsonResponse([
+                'success' => false,
+                'errors' => $validator->errors()
+            ], 422);
+        }
+
+        if ($request->input('channel') == 'phone') {
+            return $this->sendVerificationToPhone($request->input('phone'));
+        }
+
+        return new JsonResponse([
+            'success' => false,
+            'errors' => [
+                'channel' => 'invalid delivery channel'
+            ]
+        ]);
+    }
+
+    /**
+     * Send verification code to phone number
+     *
+     * @param string $phone
+     * @return JsonResponse
+     */
+    protected function sendVerificationToPhone(string $phone): JsonResponse
+    {
+
+        try {
+            $this->sendPhoneVerificationCodeService->handle($phone);
+        } catch (Exception) {
+            return new JsonResponse([
+                'success' => false
+            ], 400);
+        }
+
+        return new JsonResponse([
+            'success' => true,
+            'data' => [
+                'phone' => $phone
+            ]
+        ]);
+    }
+}
