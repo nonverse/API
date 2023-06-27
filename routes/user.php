0259b26e735cccc89b1a905cf5d8036f7a13aaf2
Isuru <isuru2003a@gmail.com>
2023-06-28 03:21:18 +1000

    Add route to update user's password

--- routes/user.php
+++ routes/user.php
@@ -32,17 +32,20 @@ Route::group(['middleware' => 'auth:api'], function () {
 
     // User security routes
     Route::prefix('/security')->group(function () {
-        // User recovery routes
-        Route::prefix('/recovery')->group(function () {
-            // Get user recovery details
-            Route::get('/', [\App\Http\Controllers\User\RecoveryController::class, 'get']);
-        });
-
         // User Two-Step routes
         Route::prefix('/two-step')->group(function () {
             // Get Two-Step setup data
             Route::get('/', [\App\Http\Controllers\User\TwoFactorController::class, 'get']);
             Route::post('/', [\App\Http\Controllers\User\TwoFactorController::class, 'enable'])->middleware('confirmed:update_two_step_login');
+        });
+
+        // Update password
+        Route::post('/password', [\App\Http\Controllers\User\PasswordController::class, 'update'])->middleware('confirmed:update_password');
+
+        // User recovery routes
+        Route::prefix('/recovery')->group(function () {
+            // Get user recovery details
+            Route::get('/', [\App\Http\Controllers\User\RecoveryController::class, 'get']);
         });
     });
 
