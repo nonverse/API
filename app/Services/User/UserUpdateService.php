b4f84a2dab2de84ff31073978bdcfc334ae2c69b
Isuru <isuru2003a@gmail.com>
2023-06-27 19:56:14 +1000

    Remove unwanted stuff in UserUpdateService

--- app/Services/User/UserUpdateService.php
+++ app/Services/User/UserUpdateService.php
@@ -41,48 +41,10 @@ class UserUpdateService
         $user = $this->repository->get($uuid);
 
         /**
-         * Create new password hash if required
-         */
-        if (Arr::has($data, 'password')) {
-            $data['password'] = $this->hasher->make($data['password']);
-        }
-
-        /**
          * Format date of birth to Carbon time
          */
         if (Arr::has($data, 'dob')) {
             $data['dob'] = CarbonImmutable::parse($data['dob'])->format('Y-m-d');
-        }
-
-        /**
-         * Resend email verification if required
-         */
-        if (Arr::has($data, 'email') && $data['email'] !== $user['email']) {
-            /*
-             * This will update the user's email in memory for the purpose of sending the
-             * verification email. No data has been persisted to the database yet
-             */
-            $user->fill([
-                'email' => $data['email']
-            ]);
-
-            try {
-                /**
-                 * If email verification is successfully sent un-verify previous email
-                 */
-                $user->sendEmailVerificationNotification();
-
-                $data = [
-                    ...$data,
-                    'email_verified_at' => null
-                ];
-            }
-            catch (Exception) {
-                /**
-                 * Don't update user's email if unable to send verification
-                 */
-                Arr::forget($data, 'email');
-            }
         }
 
         /**
