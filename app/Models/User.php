58cbe377d13e5dba41986de82c7b7cabd9e1e951
Isuru <isuru2003a@gmail.com>
2023-06-28 23:12:41 +1000

    Hide pin on outgoing responses

--- app/Models/User.php
+++ app/Models/User.php
@@ -62,6 +62,7 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $hidden = [
         'password',
         'remember_token',
+        'pin',
         'totp_secret',
         'totp_recovery_token',
     ];
