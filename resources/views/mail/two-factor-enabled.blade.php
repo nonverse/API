c10feb4bf6e61e60e030275e996648ce70e7c797
Isuru <isuru2003a@gmail.com>
2023-07-01 02:48:02 +1000

    Remove unwanted 'http://' in mail template

--- resources/views/mail/two-factor-enabled.blade.php
+++ resources/views/mail/two-factor-enabled.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
 # Hello {{$name}},
 
-Two-Factor Authentication (2FA) has been enabled on your account.
+Two-Step login has been enabled on your account.
 
 ## Your account recovery token is below
 
@@ -12,7 +12,7 @@ Two-Factor Authentication (2FA) has been enabled on your account.
 In the event that you lose access to your authenticator app, your recovery token will be required to regain access to your account
 
 @component('mail::subcopy')
-    If you did not enable 2FA for your account, please use your recovery token to login and <a href="http://{{env('BASE_APP')}}/account/support" target="_blank" rel="noreferrer">contact support</a> immediately
+    If you did not enable Two-Step login for your account, please use your recovery token to login and <a href="{{env('ACCOUNT_APP_URL')}}/account/support" target="_blank" rel="noreferrer">contact support</a> immediately
 @endcomponent
 
 With Love,<br>
