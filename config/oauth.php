4b53382ef3a9e775db299212c2d3673885c25692
Isuru <isuru2003a@gmail.com>
2023-07-01 16:04:39 +1000

    Temporarily import private key into API. After I have tested that everything works, I will use a separate key pair for JWTs that are not related to OAuth

--- config/oauth.php
+++ config/oauth.php
@@ -2,6 +2,12 @@
 
 return [
     /**
+     * RS256 Private key file
+     */
+    'private_key' => file_get_contents('../storage/oauth-private.key'),
+    //TODO OAuth should use it's own keys. JWTs that are not related to OAuth should be signed using a separate key
+
+    /**
      * RS256 Public key file
      */
     'public_key' => file_get_contents('../storage/oauth-public.key'),
