ef690339368bbcfd82aa92b34557dbfbdd90f0da
Isuru <isuru2003a@gmail.com>
2023-07-13 16:18:53 +1000

    If the recovery e-mail is owned by the user send a IP locked verification e-mail

--- app/Services/User/UsesRecovery.php
+++ app/Services/User/UsesRecovery.php
@@ -3,7 +3,10 @@
 namespace App\Services\User;
 
 use App\Models\Recovery;
+use App\Notifications\VerifyEmail;
 use Carbon\CarbonImmutable;
+use Firebase\JWT\JWT;
+use Illuminate\Support\Facades\Notification;
 
 trait UsesRecovery
 {
@@ -27,7 +30,29 @@ trait UsesRecovery
         $recovery->email = $email;
         $recovery->email_belongs_to_user = (int)$owned;
 
-        $recovery->isDirty() ?: $recovery->save();
+        $recovery->save();
+
+        /**
+         * If the recovery email is owned by the user
+         */
+        if ($owned) {
+            $payload = [
+                'iss' => env('APP_URL'),
+                'sub' => 'r-' . $this->uuid,
+                'aud' => env('APP_URL'),
+                'exp' => time() + 24 * 60 * 60,
+                'iat' => time(),
+                'ip' => $_SERVER['REMOTE_ADDR'],
+                'email' => $email
+            ];
+
+            $jwt = JWT::encode($payload, config('oauth.private_key'), 'RS256');
+
+            /**
+             * Send verification e-mail with ip verification enabled
+             */
+            Notification::route('mail', $email)->notify(new VerifyEmail($this, $jwt));
+        }
     }
 
     /**
@@ -49,6 +74,6 @@ trait UsesRecovery
         $recovery->phone = $phone;
         $recovery->phone_verified_at = CarbonImmutable::now();
 
-        $recovery->isDirty() ?: $recovery->save();
+        $recovery->save();
     }
 }
