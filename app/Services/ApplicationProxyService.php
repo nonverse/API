f1bfd9907a08c56d4600e926bd06c303945eab1b
isuru <isuru2003a@gmail.com>
2024-02-14 01:10:30 +1100

    Create service to issue signed token to access sub APIs. These APIs have no route protection and rely on protections to be run on the API server before the request is forwarded

---
+++ app/Services/ApplicationProxyService.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\User;
+use Firebase\JWT\JWT;
+
+class ApplicationProxyService
+{
+    public function createSignedToken(string $target, User $user = null): string
+    {
+        $payload = [
+            'sub' => $user?->uuid,
+            'iss' => env('APP_URL'),
+            'aud' => 'https://' . $target . '.nonverse.test/',
+            'iat' => time(),
+            'exp' => time() + 60
+        ];
+
+        /**
+         * Create new access token
+         */
+        return JWT::encode($payload, config('oauth.private_key'), 'RS256');
+    }
+}
