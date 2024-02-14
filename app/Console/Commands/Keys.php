30021144acc8e5ee1b2cff11c24af380097b89e4
isuru <isuru2003a@gmail.com>
2024-02-14 14:32:28 +1100

    Create command to generate API keys

---
+++ app/Console/Commands/Keys.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use phpseclib3\Crypt\RSA;
+
+class Keys extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'api:keys';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate Api encryption keys';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $publicKey = './storage/api-public.key';
+        $privateKey = './storage/api-private.key';
+
+        $key = RSA::createKey(4096);
+
+        file_put_contents($publicKey, (string)$key->getPublicKey());
+        file_put_contents($privateKey, (string)$key);
+
+        return Command::SUCCESS;
+    }
+}
