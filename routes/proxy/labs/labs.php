2e1e1f421574c0618e960be52e529983f0e7618f
isuru <isuru2003a@gmail.com>
2024-02-20 18:41:56 +1100

    Remove redundant middleware

--- routes/proxy/labs/labs.php
+++ routes/proxy/labs/labs.php
@@ -18,5 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [\App\Http\Controllers\Proxy\LabsProxyController::class, 'forward']);
 
 // Minecraft (Auth Required)
-Route::middleware('auth:api')->prefix('minecraft')->group(base_path('routes/proxy/labs/minecraft.php'));
+Route::prefix('minecraft')->group(base_path('routes/proxy/labs/minecraft.php'));
 
