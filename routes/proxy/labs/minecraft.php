97ce76e2aa07d8f02357f4c616956622629d870b
isuru <isuru2003a@gmail.com>
2024-02-23 01:56:14 +1100

    Update minecraft routes

--- routes/proxy/labs/minecraft.php
+++ routes/proxy/labs/minecraft.php
@@ -16,5 +16,7 @@ use Illuminate\Support\Facades\Route;
  */
 Route::middleware('auth:api')->prefix('profile')->group(function() {
     Route::post('/', [\App\Http\Controllers\Proxy\LabsProxyController::class, 'forward'])->middleware('scope:labs.*');
-    Route::get('/', [\App\Http\Controllers\Proxy\LabsProxyController::class, 'forward'])->middleware('scope:labs.*');
+    Route::get('/', [\App\Http\Controllers\Proxy\LabsProxyController::class, 'forward'])->middleware('scope:labs.mc.profile.read');
+
+    Route::post('/send-verification',[\App\Http\Controllers\Proxy\LabsProxyController::class, 'forward'])->middleware('scope:labs.*');
 });
