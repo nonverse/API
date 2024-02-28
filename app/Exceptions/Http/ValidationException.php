5129814e5d3e6442070876686dd71b1c738cc290
isuru <isuru2003a@gmail.com>
2024-02-28 13:57:04 +1100

    Create ValidationException.php

---
+++ app/Exceptions/Http/ValidationException.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Exceptions\Http;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+class ValidationException extends HttpException
+{
+    private \Illuminate\Validation\Validator $validator;
+
+    public function __construct($validator) {
+        parent::__construct(422);
+        $this->validator = $validator;
+    }
+
+    public function getResponse(): JsonResponse
+    {
+        return new JsonResponse([
+            'success' => false,
+            'errors' => $this->validator->errors()
+        ], $this->getStatusCode());
+    }
+
+    public function render(): JsonResponse
+    {
+        return $this->getResponse();
+    }
+}
